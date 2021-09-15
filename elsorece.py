fbeb9bd99d7db5b75ef00c1253b922ac5c5851fc
blzsBalint <90310465+blzsBalint@users.noreply.github.com>
2021-09-15 08:32:26 +0000

    ez az első receptem

---
+++ elsorece.py
@@ -0,0 +1,9 @@
+def rantott_hus (szosz_type):
+  print('Kloffold ki a húst')
+  print('Készíts elő egy-egy tányérnyi lisztet,felvert tojást és prézlit')
+  print('Forgasd meg a húst a helyes sorrendbe')
+  print('Sorrend:Tojás,liszt,prézli')
+  print('Süsd ki olajban')
+  print('Válasz hozzá szószt', szosz_type)
+  
+rantott_hus('ketchup')
