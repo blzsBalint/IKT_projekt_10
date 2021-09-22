683a13820b47b04d11339474c5735760e842a3b3
blzsBalint <90310465+blzsBalint@users.noreply.github.com>
2021-09-22 10:35:05 +0200

    Add files via upload

---
+++ szendvics.py
@@ -0,0 +1,9 @@
+def pongonal_gazdagsagvan (inni):
+  print('Kend meg a kenyeret kacsazsírral meg egy kis kenőmájjal')
+  print('tegyél bele rántotthúst')
+  print('Tegyél rá ketchupot,mustárt és végül majonézt')
+  print('Csak ,hogy meglegyen bolondítva egy kevéske bacon és rántott hagymakarika')
+  print('Ezt az egyészet igényesen tálaljuk serpenyőben pirított hamburger buciban')
+  print('Válasz hozzá innivalót', inni)
+  
+pongonal_gazdagsagvan('cola')
