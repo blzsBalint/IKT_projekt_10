98e9f6dee026722eec219806c0dfff22230e7f7d
blzsBalint <90310465+blzsBalint@users.noreply.github.com>
2021-10-06 09:45:02 +0200

    sp.gyakorlás

---
+++ spgyakorlás.py
@@ -0,0 +1,45 @@
+print("Feladat 1")
+nev= input ('Hogy hívnak ? ')
+print ('Szia ', nev  )
+
+print ("Feladat 2")
+print ("**********")
+
+print ("Feladat 3")
+print ("*o*o*o*o*o")
+print ("*o*o*o*o*o")
+
+print ("Feladat 4")
+a= int (input ('a = '))
+b= int (input ('b = '))
+print ( a + b )
+
+print ("Feladat 5")
+a= int (input ('a = '))
+b= int (input ('b = '))
+c= int (input ('c = '))
+print ( a + b + c )
+
+print ("Feladat 6")
+a= int (input ('a = '))
+b= int (input ('b = '))
+print ( a - b )
+
+print ("Feladat 7")
+a= int (input ('a = '))
+b= int (input ('b = '))
+print ( a * b )
+
+print ("Feladat 8")
+a= int (input ('a = '))
+b= int (input ('b = '))
+print ( a / b )
+
+
+
+
+
+
+
+
+
