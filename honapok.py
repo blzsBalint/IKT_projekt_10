a5d99a44936acd27ee292bd1cb8c1cebdf7d09ca
blzsBalint <90310465+blzsBalint@users.noreply.github.com>
2022-01-21 12:12:06 +0100

    Add files via upload

---
+++ honapok.py
@@ -0,0 +1,12 @@
+honapok=["Január","Február","Március","Április"]
+#Január=0, Február=1, Március=2, Április=3
+print(honapok[0])
+print(honapok[1])
+print(honapok[2])
+print(honapok[3])
+print(len(honapok))
+print(",".join(honapok))
+print(honapok[1:3])
+print(honapok[:2])
+print(honapok[2:])
+print(honapok[-1])
